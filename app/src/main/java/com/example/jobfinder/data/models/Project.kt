31268d31d58aafc6dca5e595b4d773c55b2db191
Danil <razdobarov.da@students.dvfu.ru>
2022-11-26 21:32:25 +1000

    fix

--- app/src/main/java/com/example/jobfinder/data/models/Project.kt
+++ app/src/main/java/com/example/jobfinder/data/models/Project.kt
@@ -13,9 +13,6 @@ data class Project(
     val state: Boolean = true,
     val creatorId: String = "",
     val salary: String = "",
-<<<<<<< HEAD
     val employer: String = ""
-=======
-    val employer:String = ""
->>>>>>> af1e48ab76a40d235ba708bae18228b1770be10f
+
 ): Serializable
